8e8cac9ddffd38e52479f9fa331b5ce84086dad6
Sergio Trejo <alejandrotrejocode@gmail.com>
2021-02-20 13:00:05 -0500

    ✔ v1.0.2 Dynamic Static Export

--- src/pages/details/[id].jsx
+++ src/pages/details/[id].jsx
@@ -4,80 +4,52 @@
  * @description Job details page /details/:id
 */
 
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
 import Layout   from 'components/Layout';
 import JobHead  from 'components/Jobs/JobHead';
-import Progress from 'components/Progress';
 import ContainerDetails from 'components/Containers/Jobs/Details';
 
 
-const PageIndex = () => {
+export async function getStaticPaths() {
 
-    const router = useRouter();
-    const [jobs, setJobs] = useState(false);
-    const [index, setIndex] = useState(null);
-    const [details, setDetails] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
+    const response = await fetch('https://talent.woonkly.com/static/data.json');
+    const json = await response.json();
 
+    return {
+        paths: json?.jobs?.map(jobsId => {
+            return {
+                params: {
+                    id: `${jobsId.id}`
+                }
+            }
+        }), fallback: false
+    }    
 
-    useEffect(() => {
+};
 
-        if (router?.query?.id) {
+export async function getStaticProps({ params }) {
 
-            const { query } = router;
-            setIndex(Number(query.id));
+    const response = await fetch('https://talent.woonkly.com/static/data.json');
+    const json = await response.json();
+    const details = json?.jobs?.find(item => Number(item.id) === Number(params.id));
 
-        }
+    return {
+        props: details
+    };
 
-    }, [router]);
+};
 
-
-    useEffect(() => {
-
-        if (jobs && index) { 
-
-            const detailed = jobs?.find(item => item.id === index);
-            detailed && setDetails(detailed);
-        }
-
-    }, [index, jobs]);
-
-
-    useEffect(() => {
-
-        const getDetails = async () => {
-
-			setIsLoading(true);
-
-			try {
-
-		  		const response = await fetch('/static/data.json');
-		  		const json = await response.json();
-		  		setJobs(json?.jobs);
-				setIsLoading(false);
-
-			} catch (error) {
-				setIsLoading(false);
-			}
-		};
-
-        getDetails();
-
-    }, []);
-
+const PageIndex = ({ title, day, remote, location, details }) => {
 
 	return (
 		<Layout>
-            <Progress isAnimating = { isLoading } />
 			<main className="page-details full column">
                 <div className="justify-center full">
                     <div className="container column">
                         <div className="justify-center">
-                            <JobHead title = { details.title } day = { details.day } remote = { details.remote } location = { details.location } />
+                            <JobHead title = { title } day = { day } remote = { remote } location = { location } />
                         </div>
                         <div className="__contents justify-center">
-                            <ContainerDetails details = { details.details } />
+                            <ContainerDetails details = { details } />
                         </div>
                     </div>
                 </div>
